4cbf0b68fc10986b4dd6a5888b3c095ba0dfe94c
Jacob Sánchez <jacobszpz@protonmail.com>
2024-05-24 10:46:01 +0100

    Add step toggle checkbox

--- components/Kryptos/Step/Base.vue
+++ components/Kryptos/Step/Base.vue
@@ -10,16 +10,31 @@ const emit = defineEmits(['change', 'remove']);
 
 const output = ref('');
 const mode = ref(0);
+const enabled = ref(true);
 
 const props = defineProps({
   index: Number,
   input: String
 });
 
+watch(toRef(() => props.input), () => {
+  if (!enabled.value) {
+    emit('change', props.input);
+  }
+});
+
 const onChange = (event) => {
   output.value = event;
-  emit('change', event);
+
+  if (enabled.value) {
+    emit('change', event);
+  }
 };
+
+watch(enabled, () => {
+  const update = enabled.value ? output.value : props.input;
+  emit('change', update);
+});
 
 const modes = [
   KryptosStepVigenere,
@@ -33,8 +48,9 @@ const modes = [
 
 <template>
 <li class="step">
-  <div class="title">
+  <div class="title" @click="enabled = !enabled">
     <button @click="emit('remove')">x</button>
+    <input v-model="enabled" type="checkbox"/>
     <h4>Step {{ index }}</h4>
   </div>
   <KryptosStepMode v-model="mode"/>
@@ -51,10 +67,17 @@ li > div.title {
   display: flex;
   flex-flow: row nowrap;
   align-items: center;
+  user-select: none;
 }
 
 li > div.title > h4 {
   margin: 0 1rem;
+}
+
+li > div.title > input {
+  background-color: black;
+  width: 1.2rem;
+  height: 1.2rem;
 }
 
 li > div.title > button {
